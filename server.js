7187e8d046783f1668580672f17fb9eb345f1ba7
shimada <gaobankirino561@gmail.com>
2025-12-18 00:47:51 +0900

    feat: customize app icon and name, fix deployment port

--- server.js
+++ server.js
@@ -1006,6 +1006,6 @@ function resolveRound(roomId) {
 }
 
 // サーバー起動
-server.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+server.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
